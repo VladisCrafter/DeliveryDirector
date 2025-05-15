2b0b932a624ade0ed26e2e2c4ab4ec4eb35f7cf7
LiukRast <darthlucavader@gmail.com>
2025-05-15 16:24:55 +0200

    Initial commit

--- src/main/java/net/liukrast/dd/PackageRewriterBlock.java
+++ src/main/java/net/liukrast/dd/PackageRewriterBlock.java
@@ -1,4 +1,22 @@
 package net.liukrast.dd;
 
-public class PackageRewriterBlock {
+import com.simibubi.create.foundation.block.IBE;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+public class PackageRewriterBlock extends Block implements IBE<PackageRewriterBlockEntity> {
+
+    public PackageRewriterBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public Class<PackageRewriterBlockEntity> getBlockEntityClass() {
+        return PackageRewriterBlockEntity.class;
+    }
+
+    @Override
+    public BlockEntityType<? extends PackageRewriterBlockEntity> getBlockEntityType() {
+        return DeliveryDirector.PRBE.get();
+    }
 }
